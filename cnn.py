73f51af39214066534ce742b9414e048cad2253e
Matthew Carrano <mc25573@gmail.com>
2019-12-15 01:14:04 -0600

    Added code for determining location of beats

--- cnn.py
+++ cnn.py
@@ -9,13 +9,19 @@ import wfdb
 import numpy as np
 import matplotlib.pyplot as plt
 
-record = wfdb.rdrecord('mit-bih-arrhythmia-database/100', sampto=1000, channels = [0])
-annotation = wfdb.rdann('mit-bih-arrhythmia-database/100', 'atr', sampto=1000)
+record = wfdb.rdrecord('mit-bih-arrhythmia-database/102', sampto=1000, channels = [0])
+anno = wfdb.rdann('mit-bih-arrhythmia-database/102', 'atr', sampto=1000)
 
-
-
-wfdb.plot_wfdb(record=record, annotation=annotation, plot_sym=True,
+wfdb.plot_wfdb(record=record, annotation=anno, plot_sym=True,
                    time_units='seconds', title='MIT-BIH Record 100',
                    figsize=(10,4), ecg_grids='all')
+#set(anno.symbol)
+# %% 
+signal,fields = wfdb.rdsamp('mit-bih-arrhythmia-database/100', channels = [0])
 
-data = wfdb.rdsamp('mit-bih-arrhythmia-database/100', sampto=1000, channels = [0])
+normal = ['/']   
+ids = np.in1d(anno.symbol,normal) # creates array of booleans, true means there is a normal beat
+
+norm_beats = anno.sample[ids] # find index of normal beat peaks
+
+norm_beats
