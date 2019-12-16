4c67f24247fe2d36cd6ebc236e62e5527407cdd4
Matthew Carrano <mc25573@gmail.com>
2019-12-15 19:13:52 -0600

    More data processing progress

--- cnn.py
+++ cnn.py
@@ -8,6 +8,7 @@ Created on Sat Dec 14 16:11:08 2019
 import wfdb
 import numpy as np
 import matplotlib.pyplot as plt
+from biosppy.signals import ecg
 
 record = wfdb.rdrecord('mit-bih-arrhythmia-database/102', sampto=1000, channels = [0])
 anno = wfdb.rdann('mit-bih-arrhythmia-database/102', 'atr', sampto=1000)
@@ -16,12 +17,33 @@ wfdb.plot_wfdb(record=record, annotation=anno, plot_sym=True,
                    time_units='seconds', title='MIT-BIH Record 100',
                    figsize=(10,4), ecg_grids='all')
 #set(anno.symbol)
-# %% 
-signal,fields = wfdb.rdsamp('mit-bih-arrhythmia-database/100', channels = [0])
+#%% DATA PROCESSING
+test_num = '100'
+sampto = 1000 # entire signal equals 650000 
 
-normal = ['/']   
-ids = np.in1d(anno.symbol,normal) # creates array of booleans, true means there is a normal beat
+signal,fields = wfdb.rdsamp('mit-bih-arrhythmia-database/' + test_num,sampto=sampto,channels = [0])
+anno = wfdb.rdann('mit-bih-arrhythmia-database/' + test_num, 'atr',sampto=sampto)
 
-norm_beats = anno.sample[ids] # find index of normal beat peaks
+set(anno.symbol)
 
-norm_beats
+#%%
+beat_types = ['N','V','/','R','L','A','E']   
+ids = np.in1d(anno.symbol,beat_types) # creates array of booleans, true means there is a normal beat
+
+
+anno_idx = anno.sample
+anno_idx = anno_idx[anno.sample < sampto-100]
+anno_idx = anno_idx[anno_idx > 100]
+
+beat_idx = anno.sample[ids] # find index of specific beat peaks
+
+beat_idx
+
+one_d_signal = []
+labels = np.array(anno.symbol) # make numpy array of labels
+labels = np.transpose(labels[ids]) # remove non-beat labels
+
+
+for i,idx in enumerate(beat_idx):
+    one_d_signal[i] = signal[idx-100:idx+100]
+    
