8d890020e7249ee1d79765018936b82171845d77
Matthew Carrano <mc25573@gmail.com>
2019-12-17 16:15:07 -0600

    Separated the CNN and data processing into separate files

--- cnn.py
+++ cnn.py
@@ -5,105 +5,6 @@ Created on Sat Dec 14 16:11:08 2019
 @author: Matthew Carrano
 """
 
-import wfdb
-import numpy as np
-import matplotlib.pyplot as plt
-from biosppy.signals import ecg
-import cv2
-
-record = wfdb.rdrecord('mit-bih-arrhythmia-database/100', sampto=1000, channels = [0])
-anno = wfdb.rdann('mit-bih-arrhythmia-database/100', 'atr', sampto=1000)
-
-wfdb.plot_wfdb(record=record, annotation=anno, plot_sym=True,
-                   time_units='seconds', title='MIT-BIH Record 100',
-                   figsize=(10,4), ecg_grids='all')
-#set(anno.symbol)
-#%% DATA PROCESSING
-import wfdb
-import numpy as np
-import matplotlib.pyplot as plt
-from biosppy.signals import ecg
-import cv2
-
-#tr_rec = ['101','106','108','109','112','114','115','116','118','119','122','124','201','203','205','207','208','209','215','220','223','230'] # training records
-#tst_rec = ['100','103','105','111','113','117','121','123','200','202','210','212','213','214','219','221','222','228','231','232','233'] # test records
-tst_rec = ['101','106','108','109','112','114','115','116','124','201','203','205','207','208'] 
-tst_im = None
-tst_labels = None
-
-for k,num in enumerate(tst_rec):    
-    #test_num = '100'
-    sampto = 650000 # entire signal equals 650000 
-    
-    signal,fields = wfdb.rdsamp('mit-bih-arrhythmia-database/'+num,sampto=sampto,channels = [0])
-    anno = wfdb.rdann('mit-bih-arrhythmia-database/'+num, 'atr',sampto=sampto)
-    signal = np.array(signal) # convert to numpy array
-    
-    set(anno.symbol)
-    
-    beat_types = ['N','V','R','L','A','E'] # beat types the network will classify
-    ids = np.in1d(anno.symbol,beat_types) # creates array of booleans, 'True' means there is a normal beat
-    labels = np.array(anno.symbol) # make numpy array of labels
-    
-    # remove beats starting before 100 samples and after end-100 samples
-    upper = sampto - 100
-    anno_idx = anno.sample
-    anno_idx = anno_idx[anno.sample < upper]
-    ids = ids[anno.sample < upper]
-    labels = labels[anno.sample < upper]
-    labels = labels[anno_idx > 100]
-    ids = ids[anno_idx > 100] 
-    anno_idx = anno_idx[anno_idx > 100] 
-    
-    labels = labels[ids] # remove "non-beat" labels
-    
-    #np.count_nonzero(labels=='N')
-    
-    beat_idx = np.array(anno_idx[ids]) # find index of specific beat peaks
-    
-    signals = np.zeros((200,len(beat_idx)))
-    
-    # each column is a beat
-    for i,idx in enumerate(beat_idx):
-        signals[:,i] = np.array(signal[idx-100:idx+100,0])
-     
-    del signal # clear to free up memory
-    
-    # Convert to image
-    
-    im_size = 128
-    im_gray = np.zeros((len(beat_idx),im_size,im_size))    
-    
-    for i in range(len(beat_idx)):
-        fig = plt.figure(frameon=False)
-        plt.plot(signals[:,i]) 
-        plt.xticks([]), plt.yticks([])
-        for spine in plt.gca().spines.values():
-            spine.set_visible(False)
-        
-        filename = 'C:/Users/Matthew/Documents/GitHub/ECG_CNN/dontcare.png' # overwrites each image
-        fig.savefig(filename)
-        plt.close() # so that it isn't storing every figure in memory
-        im = cv2.imread(filename, cv2.IMREAD_GRAYSCALE) # create image array
-        im_gray[i,:,:] = cv2.resize(im, (im_size, im_size), interpolation = cv2.INTER_LANCZOS4) # increase size
-        #cv2.imwrite(filename, im_gray)
-        #im_gray=im_gray.reshape(1,300,300,1)
-    
-    im_gray /= 255 # normalize
-    
-    # only concatenate if not first loop
-    if tst_im is None:
-        tst_im = im_gray
-        tst_labels = labels
-    else:
-        tst_im = np.concatenate((tst_im,im_gray))
-        tst_labels = np.append(tst_labels,labels)
-        
-    #im_gray = im_gray.reshape(im_gray.shape[0],im_size,im_size,1)
-
-del im_gray, im, labels, signals
-
-#%%
 from keras.utils import to_categorical
 import numpy as np
 
@@ -116,7 +17,7 @@ beat_types = ['N','V','R','L','A','E']
 
 tst_labels = np.load('../tst_labels.npy')
 tr_labels = np.load('../tr_labels.npy')
-#%%
+
 tst_labels = toInt(tst_labels,beat_types)       
 tst_labels = to_categorical(tst_labels,num_classes=6)
 
@@ -185,7 +86,7 @@ model.add(Dense(6, activation='softmax'))
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-
+# model.fit(tr_im,tr_labels,batch_size=128,)
 
 
     
---
+++ dataProcessing.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 17 16:13:47 2019
+
+@author: Matthew
+"""
+
+import wfdb
+import numpy as np
+import matplotlib.pyplot as plt
+from biosppy.signals import ecg
+import cv2
+
+record = wfdb.rdrecord('mit-bih-arrhythmia-database/100', sampto=1000, channels = [0])
+anno = wfdb.rdann('mit-bih-arrhythmia-database/100', 'atr', sampto=1000)
+
+wfdb.plot_wfdb(record=record, annotation=anno, plot_sym=True,
+                   time_units='seconds', title='MIT-BIH Record 100',
+                   figsize=(10,4), ecg_grids='all')
+#set(anno.symbol)
+#%% DATA PROCESSING
+import wfdb
+import numpy as np
+import matplotlib.pyplot as plt
+from biosppy.signals import ecg
+import cv2
+
+tr_rec = ['101','106','109','112','115','116','118','119','122','124','201','203','207','208','209','215','220','223'] # training records
+#tst_rec = ['100','103','105','111','113','117','121','123','200','202','210','212','213','214','219','221','222','228','231','232','233'] # test records
+#tst_rec = ['100','103','105','111','113','200','222','124','212','213','231','232','233'] 
+tr_im = None
+tr_labels = None
+
+for k,num in enumerate(tr_rec):    
+    sampto = 650000 # entire signal equals 650000 
+    
+    signal,fields = wfdb.rdsamp('mit-bih-arrhythmia-database/'+num,sampto=sampto,channels = [0])
+    anno = wfdb.rdann('mit-bih-arrhythmia-database/'+num, 'atr',sampto=sampto)
+    signal = np.array(signal) # convert to numpy array
+       
+    beat_types = ['N','V','R','L','A','E'] # beat types the network will classify
+    ids = np.in1d(anno.symbol,beat_types) # creates array of booleans, 'True' means there is a normal beat
+    labels = np.array(anno.symbol) # make numpy array of labels
+    
+    # remove beats starting before 100 samples and after end-100 samples
+    upper = sampto - 100
+    anno_idx = anno.sample
+    anno_idx = anno_idx[anno.sample < upper]
+    ids = ids[anno.sample < upper]
+    labels = labels[anno.sample < upper]
+    labels = labels[anno_idx > 100]
+    ids = ids[anno_idx > 100] 
+    anno_idx = anno_idx[anno_idx > 100] 
+    
+    labels = labels[ids] # remove "non-beat" labels
+    
+    beat_idx = np.array(anno_idx[ids]) # find index of specific beat peaks
+    
+    signals = np.zeros((200,len(beat_idx)))
+    
+    # each column is a beat
+    for i,idx in enumerate(beat_idx):
+        signals[:,i] = np.array(signal[idx-100:idx+100,0])
+     
+    del signal # clear to free up memory
+    
+    # Convert to image    
+    im_size = 128
+    im_gray = np.float16(np.zeros((len(beat_idx),im_size,im_size))) # float16 to save memory    
+    
+    for i in range(len(beat_idx)):
+        fig = plt.figure(frameon=False)
+        plt.plot(signals[:,i]) 
+        plt.xticks([]), plt.yticks([])
+        for spine in plt.gca().spines.values():
+            spine.set_visible(False)
+        
+        filename = 'C:/Users/Matthew/Documents/GitHub/ECG_CNN/dontcare.png' # overwrites each image
+        fig.savefig(filename)
+        plt.close() # so that it isn't storing every figure in memory
+        im = cv2.imread(filename, cv2.IMREAD_GRAYSCALE) # create image array
+        im_gray[i,:,:] = cv2.resize(im, (im_size, im_size), interpolation = cv2.INTER_LANCZOS4) # increase size
+    
+    im_gray /= 255 # normalize
+    
+    # only concatenate if not first loop
+    if tr_im is None:
+        tr_im = im_gray
+        tr_labels = labels
+    else:
+        tr_im = np.concatenate((tr_im,im_gray))
+        tr_labels = np.append(tr_labels,labels)
+        
+    #im_gray = im_gray.reshape(im_gray.shape[0],im_size,im_size,1)
+
+del im_gray, im, labels, signals
+#np.save('tst_im.npy',tst_im)
+#np.save('tst_labels.npy',tst_labels)
